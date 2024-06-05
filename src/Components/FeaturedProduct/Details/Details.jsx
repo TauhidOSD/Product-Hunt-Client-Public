fce386a3cacffce45f2bece6b78415e8f87dee76
Tauhid hossen <tauhidhossen04@gmail.com>
2024-06-05 17:13:04 +0600

    review data post on MongoDb

--- src/Components/FeaturedProduct/Details/Details.jsx
+++ src/Components/FeaturedProduct/Details/Details.jsx
@@ -1,5 +1,7 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
 import { useLoaderData, useParams } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const Details = () => {
 
@@ -15,37 +17,148 @@ const Details = () => {
     setProduct(findCart);
   }, [_id, details]);
     
-  
+
+
+  const handleAddReview = (event) => {
+    event.preventDefault();
+
+    const form = event.target;
+    const name = form.name.value;
+    const image = form.image.value;
+    const comment = form.comment.value;
    
+    
+
+    const review = {
+        name,
+        image,
+        comment
+       
+        
+    }
+    console.log(review);
+
+    axios.post("http://localhost:5000/review",review,
+)
+// .then(res =>res.json())
+.then(data=>{
+    console.log(data);
+    if(data?.data?.insertedId){
+        Swal.fire({
+            title: 'Success!',
+            text: 'User added Successfully',
+            icon: 'success',
+            confirmButtonText: 'Cool'
+          })
+    }
+})
+  
+}
 
 
     return (
-        <div className="">
-        <section className="  bg-white dark:bg-gray-900">
-   <div className="container px-4  mx-auto">
-     
-     <div className="grid grid-cols-1 gap-8 mx-auto mt-8 lg:grid-cols-1 xl:mt-10 max-w-7xl">
-       <div className="p-6 bg-gray-100 rounded-lg dark:bg-gray-800 md:p-8">
-         <p className="leading-loose text-gray-500 dark:text-gray-300">
-          {product?.Description}
-         </p>
+        <div className='flex flex-col md:flex-row justify-around gap-5  items-center min-h-[calc(100vh-306px)] md:max-w-screen-xl mx-auto '>
+      {/* Job Details */}
+      <div className='flex-1  px-4 py-7 bg-white rounded-md shadow-md md:min-h-[350px]'>
+        <div className='flex items-center justify-between'>
+          <span className='text-sm font-light text-gray-800 '>
+            Deadline: 12/08/2024
+          </span>
+          <span className='px-4 py-1 text-xs text-blue-800 uppercase bg-blue-200 rounded-full '>
+            Web Development
+          </span>
+        </div>
 
-         <div className="flex items-center mt-6">
-           <img className="object-cover rounded-full w-14 h-14" src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80" alt="Robbert" />
+        <div>
+          <h1 className='mt-2 text-3xl font-semibold text-gray-800 '>
+            Build Dynamic Website
+          </h1>
 
-           <div className="mx-4">
-             <h1 className="font-semibold text-blue-500">{product?.P_name}</h1>
-             <span className="text-sm text-gray-500 dark:text-gray-300">CTO, Robert Consultency</span>
-           </div>
-         </div>
-       </div>
+          <p className='mt-2 text-lg text-gray-600 '>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit...
+          </p>
+          <p className='mt-6 text-sm font-bold text-gray-600 '>
+            Buyer Details:
+          </p>
+          <div className='flex items-center gap-5'>
+            <div>
+              <p className='mt-2 text-sm  text-gray-600 '>Name: Jhankar Vai.</p>
+              <p className='mt-2 text-sm  text-gray-600 '>
+                Email: jhankar@mahbub.com
+              </p>
+            </div>
+            <div className='rounded-full object-cover overflow-hidden w-14 h-14'>
+              <img src='' alt='' />
+            </div>
+          </div>
+          <p className='mt-6 text-lg font-bold text-gray-600 '>
+            Range: $100 - $150
+          </p>
+        </div>
+      </div>
+      {/* Place A Bid Form */}
+      <section className='p-6 w-full  bg-white rounded-md shadow-md flex-1 md:min-h-[350px]'>
+        <h2 className='text-lg font-semibold text-gray-700 capitalize '>
+          Post Review
+        </h2>
 
-       
-     </div>
-   </div>
- </section>
-         
-     </div>
+        <form onSubmit={handleAddReview} >
+          <div className='grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2'>
+            <div>
+              <label className='text-gray-700 ' htmlFor='price'>
+                Reviewer Name
+              </label>
+              <input
+                id='price'
+                type='text'
+                name='name'
+                className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md   focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring'
+              />
+            </div>
+
+            <div>
+              <label className='text-gray-700 ' htmlFor='emailAddress'>
+               Reviewer Image
+              </label>
+              <input
+                id='emailAddress'
+                type='text'
+                name='image'
+                
+                className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md   focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring'
+              />
+            </div>
+
+            <div>
+              <label className='text-gray-700 ' htmlFor='comment'>
+                Description
+              </label>
+              <input
+                id='comment'
+                name='comment'
+                type='text'
+                className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md   focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring'
+              />
+            </div>
+            <div className='flex flex-col gap-2 '>
+              <label className='text-gray-700'></label>
+
+              {/* Date Picker Input Field */}
+            </div>
+          </div>
+
+          <div className='flex justify-end mt-6'>
+            <button
+              type='submit'
+              className='px-8 py-2.5 leading-5 text-white transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600'
+            >
+             Submit
+            </button>
+          </div>
+        </form>
+      </section>
+    </div>
+//        
     );
 };
 
